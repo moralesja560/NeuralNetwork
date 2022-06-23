7cec7df26ce27106454401d2016d536d10d8c45a
moralesja <aalberto_morales@hotmail.com>
2022-06-22 23:03:56 -0500

    first neural network finished

--- loading_neural_model1.py
+++ loading_neural_model1.py
@@ -7,4 +7,12 @@ new_model = tf.keras.models.load_model(r'C:\Users\moralesja.group\Documents\SC_R
 new_model.summary()
 
 #make a prediction using the trained model 
-print(new_model.predict(np.array([17.0])))
+
+sample = -7.0
+
+for i in range(0,20):
+	print(f"Valor ingresado a la red neuronal: {sample} resultado correcto: {sample+10}")
+	neural_guess =  round(np.double(new_model.predict(np.array([sample]))),4)
+	print(f"valor de la red neuronal {neural_guess}")
+	print(f"diferencia de valores {round((sample+10)-(neural_guess),3)}")
+	sample += 10
--- training_neural1.py
+++ training_neural1.py
@@ -1,5 +1,16 @@
 import tensorflow as tf
 import numpy as np
+import os
+import sys
+
+
+#This function sets the absolute path for the app to access its resources
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
 
 #Get the data
 #create features (or X independent data)
@@ -15,18 +26,19 @@ y = tf.constant(y)
 model = tf.keras.Sequential()
 
 model.add(tf.keras.layers.Dense(50, activation =None))
+model.add(tf.keras.layers.Dense(50, activation =None))
 model.add(tf.keras.layers.Dense(1))
 
 # Step 2 .- Compile the model
 #we can add as much layers as we want
 model.compile(loss = tf.keras.losses.mae,
-              optimizer = tf.keras.optimizers.Adam(learning_rate= 0.01),
+              optimizer = tf.keras.optimizers.Adam(learning_rate= 0.02),
               metrics = ["mae"]
               )
 #fit the model
-model.fit(tf.expand_dims(X, axis=-1),y, epochs=100)
+model.fit(tf.expand_dims(X, axis=-1),y, epochs=300)
 
-model.save(r'C:\Users\moralesja.group\Documents\SC_Repo\NeuralNetwork\savedmodel')
+model.save(resource_path(r"savedmodel"))
 
 #predict data
-print(model.predict(np.array([17.0])))
+print(model.predict(np.array([183.0])))
