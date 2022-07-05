c6cd9e83db13f858a1a9a2f0274cc85e5b53f743
moralesja <aalberto_morales@hotmail.com>
2022-07-05 05:11:40 -0500

    update

--- multi_var_NN.py
+++ multi_var_NN.py
@@ -6,9 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import sys
-import csv
 
-tf.random.set_seed(42)
+
 #This function sets the absolute path for the app to access its resources
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -16,8 +15,22 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
+def plot_call(X_train,y_train,X_test,y_test,y_pred):
+	#step 6: visualize the data 
+	plt.figure(figsize =(10,7))
+	#plot training data in blue
+	plt.scatter(X_train,y_train, c="b", label="Training Data", marker='.')
+	plt.plot(X_train,y_train)
+	plt.scatter(X_test,y_test, c="g", label="Testing Data",marker='.')
+	plt.plot(X_test,y_test)
+	plt.scatter(X_test,y_pred, c="r", label="Neural Response",marker='.')
+	plt.plot(X_test,y_pred)
+	plt.legend()
+	plt.show()
+
+
 #step 1: load data from CSV
-insurance_data = pd.read_csv (resource_path(r"resources/insurance.csv"),header=0)
+insurance_data = pd.read_csv("https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/insurance.csv")
 
 #refer to a column by using insurance_data.loc[:"age"] or  insurance_data.iloc[:,4]
 #refer to independent variable as the features and depend vars as outcome or labels.
@@ -41,17 +54,113 @@ y = insurance_one_hot["charges"]
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
 
-print(len(X),len(X_test), len(X_train))
+
+tf.random.set_seed(42)
+insurance_model = tf.keras.Sequential()
+
+insurance_model.add(tf.keras.layers.Dense(10))
+insurance_model.add(tf.keras.layers.Dense(1))
+
+# Step 2 .- Compile the model
+#we can add as much layers as we want
+insurance_model.compile(loss = tf.keras.losses.mae,
+              optimizer = tf.keras.optimizers.SGD(),
+              metrics = ["mae"]
+              )
+#fit the model
+history1 = insurance_model.fit(X_train,y_train, epochs=100,verbose=0)
 
 
-#create the model 
-insurance_model = tf.keras.Sequential([
-    tf.keras.layers.Dense(10),
-    tf.keras.layers.Dense(1)
+
+#test data set
+print(insurance_model.evaluate(X_test,y_test))
+
+
+# Set random seed
+tf.random.set_seed(42)
+
+# Add an extra layer and increase number of units
+insurance_model_2 = tf.keras.Sequential([
+  tf.keras.layers.Dense(100), # 100 units
+  tf.keras.layers.Dense(10), # 10 units
+  tf.keras.layers.Dense(1) # 1 unit (important for output layer)
 ])
 
-#2 Compile the model
-insurance_model.compile(loss=tf.keras.losses.mae, optimizer=tf.keras.optimizers.SGD(),metrics = ["mae"])
+# Compile the model
+insurance_model_2.compile(loss=tf.keras.losses.mae,
+                          optimizer=tf.keras.optimizers.Adam(), # Adam works but SGD doesn't 
+                          metrics=['mae'])
 
-insurance_model.fit(tf.expand_dims(X_train, axis=-1),y_train, epochs=100)
+# Fit the model and save the history (we can plot this)
+history2 = insurance_model_2.fit(X_train, y_train, epochs=100, verbose=0)
 
+
+#test data set
+print(insurance_model_2.evaluate(X_test,y_test))
+
+
+tf.random.set_seed(42)
+insurance_model_3 = tf.keras.Sequential()
+
+insurance_model_3.add(tf.keras.layers.Dense(100, activation = "relu"))
+insurance_model_3.add(tf.keras.layers.Dense(10, activation = "relu"))
+insurance_model_3.add(tf.keras.layers.Dense(1))
+
+# Step 2 .- Compile the model
+#we can add as much layers as we want
+insurance_model_3.compile(loss = tf.keras.losses.mae,
+              optimizer = tf.keras.optimizers.Adam(),
+              metrics = ["mae"]
+              )
+#fit the model
+history3 = insurance_model_3.fit(X_train,y_train, epochs=200,verbose=0)
+
+
+
+#test data set
+print(insurance_model_3.evaluate(X_test,y_test))
+
+
+insurance_model4 = tf.keras.Sequential()
+
+insurance_model4.add(tf.keras.layers.Dense(100, activation = "relu"))
+insurance_model4.add(tf.keras.layers.Dense(100, activation = "relu"))
+insurance_model4.add(tf.keras.layers.Dense(1))
+
+# Step 2 .- Compile the model
+#we can add as much layers as we want
+insurance_model4.compile(loss = tf.keras.losses.mae,
+              optimizer = tf.keras.optimizers.Adam(),
+              metrics = ["mae"]
+              )
+#fit the model
+history4 = insurance_model4.fit(X_train,y_train, epochs=400,verbose=0)
+
+#test data set
+print(insurance_model4.evaluate(X_test,y_test))
+
+
+#step 6: visualize the data 
+plt.figure(figsize =(10,7))
+#plot training data in blue
+plt.scatter(history3.epoch,history3.history["loss"], c="blue", label="Model 3", marker='.')
+plt.plot(history3.epoch,history3.history["loss"])
+plt.scatter(history2.epoch,history2.history["loss"], c="red", label="Model 2", marker='.')
+plt.plot(history2.epoch,history2.history["loss"])
+plt.scatter(history1.epoch,history1.history["loss"], c="green", label="Model 1", marker='.')
+plt.plot(history1.epoch,history1.history["loss"])
+plt.scatter(history4.epoch,history4.history["loss"], c="yellow", label="Model 4", marker='.')
+plt.plot(history4.epoch,history4.history["loss"])
+
+plt.legend()
+plt.show()
+
+
+y_pred_1 = insurance_model.predict(X_test)
+y_pred_2 = insurance_model_2.predict(X_test)
+y_pred_3 = insurance_model_3.predict(X_test)
+y_pred_4 = insurance_model4.predict(X_test)
+
+results = pd.DataFrame(list(zip(y_test,y_pred_1,y_pred_2,y_pred_3,y_pred_4)),columns=["Valor Real","Modelo 1","Modelo 2","Modelo 3","Modelo 4"])
+
+print(results.to_string())
